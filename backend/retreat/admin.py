bd1e7c9a1b73a8b9b0145dba28824c22cf1727f5
Edward Iyanda <edwardiyanda60@gmail.com>
2026-06-13 15:26:40 +0100

    feat: Update admin registration to exclude Program from the default model list

--- backend/retreat/admin.py
+++ backend/retreat/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Participant, RetreatSession, Attendance, Program
-admin.site.register([Participant, RetreatSession, Attendance, Program])
+admin.site.register([Participant, RetreatSession, Attendance])
+
 
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
